c1bb38464cb03565f89bdb7b1fb7eead8fd164c6
Kimberly Palmer <kimberlylpalmer@gmail.com>
2023-11-21 03:21:58 -0800

    Lab complete

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,12 +1,17 @@
 import React from "react";
 import ProjectItem from "./ProjectItem";
+// import user from "./data/user";
 
 function ProjectList({ projects }) {
   console.log(projects);
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {projects.map((project) => (
+          <ProjectItem key={project.id} {...project} />
+        ))}
+      </div>
     </div>
   );
 }
